a5af9b4f1bc4caad8ed447b10815b13f53ac5815
lucas <lucassalna06gmail.com>
2025-07-16 11:44:26 -0500

    arreglo de url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
 import { useAuthStore } from '../store/auth'
 
-// Detectar si estamos en producción
-const isProd = import.meta.env.PROD
-
 const api = axios.create({
-  baseURL: isProd
-    ? 'http://ec2-3-140-254-107.us-east-2.compute.amazonaws.com/api' // API real en producción
-    : '/api', // Esto sigue funcionando en desarrollo por el proxy
+  baseURL: '/api/', 
 })
 
 api.interceptors.request.use((config) => {
